cbc3b5710ea89fcdb90e3c3e2b7bc1dd56e9a8a8
Eren Bulur <bulureren76@gmail.com>
2026-08-15 14:03:02 +0300

    Clean build artifacts - Add auto-cleanup to BUILD.bat, remove old files

--- BUILD.bat
+++ BUILD.bat
@@ -1,9 +1,17 @@
 @echo off
 cd /d "%~dp0"
+echo Cleaning old build files...
+if exist build rmdir /s /q build
+if exist dist rmdir /s /q dist
+if exist __pycache__ rmdir /s /q __pycache__
+if exist _internal rmdir /s /q _internal
 echo Building Launcher...
 python -m PyInstaller --noconfirm Launcher.spec
 echo Building MerlinMakro...
 python -m PyInstaller --noconfirm MerlinMakro.spec
 copy /Y dist\Launcher.exe Launcher.exe
 copy /Y dist\MerlinMakro.exe MerlinMakro.exe
+echo Cleaning build directories...
+if exist build rmdir /s /q build
+if exist dist rmdir /s /q dist
 echo Done. EXE files updated in project root.
